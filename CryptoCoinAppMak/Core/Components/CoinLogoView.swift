accac15d1f510f81aa5bbb8ba77ba9d6a43684f4
stackmak21 <stackmak21@gmail.com>
2023-11-26 10:23:16 +0200

    updated

---
+++ CryptoCoinAppMak/Core/Components/CoinLogoView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinLogoView.swift
+//  CryptoCoinAppMak
+//
+//  Created by Paris Makris on 25/11/23.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinLogoView()
+}
---
+++ CryptoCoinAppMak/Core/Components/XMarkButton.swift
@@ -0,0 +1,18 @@
+//
+//  XMarkButton.swift
+//  CryptoCoinAppMak
+//
+//  Created by Paris Makris on 25/11/23.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XMarkButton()
+}
